3200aeccf08015937b53172e0637ebb7bd1a3062
Remicck <remicck@gmail.com>
2024-11-21 13:55:47 +0900

    sidebarを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,21 @@ import type { PropsWithChildren, ReactNode } from "react";
 import { Footer } from "./_components/Footer";
 import { Header } from "./_components/Header";
 import "./globals.css";
+import { AppSidebar } from "@/app/_components/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/app/_components/ui/breadcrumb";
+import { Separator } from "@/app/_components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/app/_components/ui/sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,9 +47,38 @@ export default async function Layout({ dialog, children }: Props) {
           "has-[dialog[open]]:overflow-hidden",
         ].join(" ")}
       >
-        <Header />
-        <main className="py-4 px-8 flex-1 overflow-y-auto">{children}</main>
-        <Footer />
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+              <div className="flex items-center gap-2 px-4">
+                <SidebarTrigger className="-ml-1" />
+                <Separator orientation="vertical" className="mr-2 h-4" />
+                <Breadcrumb>
+                  <BreadcrumbList>
+                    <BreadcrumbItem className="hidden md:block">
+                      <BreadcrumbLink href="#">
+                        Building Your Application
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator className="hidden md:block" />
+                    <BreadcrumbItem>
+                      <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                    </BreadcrumbItem>
+                  </BreadcrumbList>
+                </Breadcrumb>
+              </div>
+            </header>
+            <div className="p-4 pt-0">
+              <Header />
+              <main className="py-4 px-8 flex-1 overflow-y-auto">
+                {children}
+              </main>
+              <Footer />
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
+
         {dialog}
       </body>
     </html>
